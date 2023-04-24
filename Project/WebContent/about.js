e9f3e30aed1550f543373d3cd984acaab8c01962
psh0121 <tngus2057@gmail.com>
2023-04-24 19:12:44 +0000

    [소개] section0 애니메이션 적용 및 디자인 수정

--- Project/WebContent/about.js
+++ Project/WebContent/about.js
@@ -1,5 +1,9 @@
 (() => {
 
+    let currentScrollY = 0;     // 현재 scrollY 위치값
+    let currentSection = 0;     // 현재 섹션 위치
+    let sectionYOffset = 0;     // 현재 섹션에 따른 scrollY 상대값
+
     // sectionSet: n번째 섹션에 대한 정보집합
     const sectionSet = [
         // section0
@@ -7,8 +11,12 @@
             height: 0,
             multiplayValue: 1,
             elemInfo: {
-                section: document.querySelector('.section0')
-            }
+                section: document.querySelector('.section0'),
+                message: document.querySelector('.section0-contents-message'),
+                arrow: document.querySelector('.section0-arrow')
+            },
+            opacitySettingsValue: [0, 1],
+            translateYSettingsValue: [3, 0]
         },
 
         // section1
@@ -64,6 +72,98 @@
         }
     }
 
+    // getCurrentSection: scrollY 위치에 따른 현재 섹션위치 구하기
+    // - parameter: x
+    // - return: 현재 섹션 값
+    const getCurrentSection = function() {
+        let currentSection = 0;
+        let sum = 0;
+        let temp = 0;
+
+        for(let i = 0; i < sectionSet.length; i++){
+            sum += sectionSet[i].height;
+
+            if(currentScrollY <= sum){
+                currentSection = temp;
+                break;
+            }
+            temp++;
+        }
+        return currentSection;
+    }
+
+    // getSectionYOffset: 현재 섹션 위치에 따른 scrollY의 상대값 구하기
+    // - parameter: x
+    // - return: 현재섹션 위치에 따른 scrollY 상대값
+    const getSectionYOffset = function() {
+        let yOffset = currentScrollY;
+
+        for(let i = 0; i < currentSection; i++){
+            yOffset -= sectionSet[i].height;
+        }
+        return yOffset;
+    }
+
+    // section0Animation: section0에서 발생되는 애니메이션
+    // - parameter: x
+    // - return: x
+    const section0Animation = function() {
+        const msg = sectionSet[0].elemInfo.message;
+        const arrow = sectionSet[0].elemInfo.arrow;
+
+        let opValue = sectionSet[0].opacitySettingsValue[0];
+        let yValue = sectionSet[0].translateYSettingsValue[0];
+        let tid1;
+        let tid2;
+        let appearFlag = true;
+
+        tid1 = setInterval(() => {
+            msg.style.opacity = opValue;
+            arrow.style.opacity = opValue;
+            opValue += 0.05;
+
+            msg.style.transform = `translateY(${yValue}%)`;
+            arrow.style.transform = `translateY(${yValue}%)`;
+            yValue -= 0.05;
+
+            if(
+                (opValue >= sectionSet[0].opacitySettingsValue[1]) &&
+                (yValue <= sectionSet[0].translateYSettingsValue[1])
+            ){
+                clearInterval(tid1);
+                opValue = 0;
+                yValue = 3;
+                appearFlag = false;
+
+                tid2 = setInterval(() => {
+                    if(appearFlag){
+                        arrow.style.opacity = opValue;
+                        opValue += 0.05;
+
+                        if(opValue >= sectionSet[0].opacitySettingsValue[1]){
+                            appearFlag = false;
+                        }
+                    }
+                    else{
+                        arrow.style.opacity = opValue;
+                        opValue -= 0.05;
+
+                        if(opValue <= sectionSet[0].opacitySettingsValue[0]){
+                            appearFlag = true;
+                        }
+                    }
+                }, 40);
+            }
+        }, 10);
+    }
+
+    // loadAnimation: 화면이 로드될 때에 한번만 발생되는 애니메이션
+    // - parameter: x
+    // - return: x
+    const loadAnimation = function() {
+        section0Animation();
+    }
+
     /////////////////////////////////////////////////////////
     // 이벤트 핸들러
 
@@ -74,16 +174,35 @@
             window.scrollTo(0, 0);
         }, 100);
 
+        currentScrollY = window.scrollY;
+        currentSection = getCurrentSection();
+        sectionYOffset = getSectionYOffset();
+
         setLayout();
+        loadAnimation();
     })
 
     // 스크롤을 진행했을시에 발생되는 이벤트
     window.addEventListener("scroll", () => {
+        currentScrollY = window.scrollY;
+        currentSection = getCurrentSection();
+        sectionYOffset = getSectionYOffset();
+        
         setBlur();
     })
 
     // resize시 발생되는 이벤트
     window.addEventListener("resize", () => {
+        currentScrollY = window.scrollY;
+        currentSection = getCurrentSection();
+        sectionYOffset = getSectionYOffset();
+
         setLayout();
     })
+
+    // section0 이벤트
+    document.querySelector('.section0-arrow').addEventListener("click", () => {
+        window.scrollTo({left: 0, top: sectionSet[0].height, behavior: "smooth"});
+    })
+
 })();
