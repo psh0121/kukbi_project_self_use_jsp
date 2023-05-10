f3af2631e67e4a0839b92cd6c99207c41c0035b1
psh0121 <tngus2057@gmail.com>
2023-05-10 19:20:44 +0000

    [자주하는질문] nav 스크롤이벤트 및 디자인 설정

--- Project/WebContent/fqa.js
+++ Project/WebContent/fqa.js
@@ -1,3 +1,30 @@
 (() => {
-	console.log('hi');
+	
+	/////////////////////////////////////////////////////////
+	// 일반함수
+	
+	// appearNavLine: 스크롤 발생시 nav 하단에 회색줄이 보이도록 설정
+    // - parameter: x
+    // - return: x
+    const appearNavLine = function() {
+        if(window.scrollY === 0) {
+            document.querySelector('nav').style.borderBottom = null;
+        }
+        else {
+            document.querySelector('nav').style.borderBottom = '1px solid lightgray';
+        }
+    }
+	
+	/////////////////////////////////////////////////////////
+	// 이벤트 핸들러
+	
+	// 로딩된 후에 발생되는 이벤트
+    window.addEventListener("load", () => {
+        document.querySelector('.global-nav').id = 'fqa-nav';
+    })
+
+    // 스크롤을 진행했을시에 발생되는 이벤트
+    window.addEventListener("scroll", () => {
+        appearNavLine();
+    })
 })();
