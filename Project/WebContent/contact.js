1ee74a0669f79a04e745e451f714886b8af94e60
psh0121 <tngus2057@gmail.com>
2023-06-27 17:23:21 +0900

    [고객센터] 공지/분류/신고 버튼 클릭시 해당 데이터만 보이도록 설정

--- Project/WebContent/contact.js
+++ Project/WebContent/contact.js
@@ -13,6 +13,27 @@
             document.querySelector('nav').style.borderBottom = '1px solid lightgray';
         }
     }
+
+    // showInfo: 입력받은 분류항목만 화면에 표시될 수 있도록 함
+    // - parameter: 분류항목 (공지/문의/신고)
+    // - return: x
+    const showInfo = function(category) {
+        let categoryObj = {
+            'notice': document.querySelector('.notice-content'),
+            'inquiry': document.querySelector('.inquiry-content'),
+            'report': document.querySelector('.report-content')
+        };
+
+        for(let [key, doc] of Object.entries(categoryObj)) {
+            if(key === category) {
+                doc.style.display = '';
+            }
+
+            else {
+                doc.style.display = 'none';
+            }
+        }
+    }
 	
 	/////////////////////////////////////////////////////////
 	// 이벤트 핸들러
@@ -26,7 +47,23 @@
 	window.addEventListener("scroll", () => {
         appearNavLine();
     })
-    
+
+    // 공지, 문의, 신고버튼 이벤트
+    document.querySelector(".notice-btn").addEventListener("click", () => {
+        console.log('공지');
+        showInfo('notice');
+    })
+
+    document.querySelector(".inquiry-btn").addEventListener("click", () => {
+        console.log('문의');
+        showInfo('inquiry');
+    })
+
+    document.querySelector(".report-btn").addEventListener("click", () => {
+        console.log('신고');
+        showInfo('report');
+    })
+
     // 글쓰기 버튼 이벤트
     document.querySelector('#btn-write').addEventListener("click", () => {
         window.location.href = './contact_text.jsp';
